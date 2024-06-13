151308c5ae0f35626eea631eededa8514d168209
Bartlomiej Sabiniok <barteksabcio@gmail.com>
2024-06-13 11:15:24 +0200

    sass config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import Navbar from "./components/Navbar.tsx";
-// import Header from "./components/Header";
-// import Main from "./components/Main";
-// import Footer from "./components/Footer";
+import './styles/_main.scss'
 
 function App() {
-  return (
-      
-    <div className='overflow-x-hidden'>
-      <Navbar/>
-      {/* background */}
-      <video src="./src/assets/bacground.mp4" loop autoPlay muted className='object-cover absolute h-screen w-screen -z-10 top-0 left-0'></video>
 
-      {/* content */}
-  </div>
-  );
+  return (
+   <><h1>
+    hello World
+    </h1></>
+  )
 }
 
-export default App;
+export default App
